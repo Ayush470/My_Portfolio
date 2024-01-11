6936dd196be19e6210e7358b7f4ef90b8aeac715
Ayush_Prajapati <ap4701443@gmail.com>
2024-01-11 17:09:04 +0530

    adding changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,4 @@
 import "./FormStyles.css";
-import dotenv from "dotenv";
 
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
@@ -11,10 +10,10 @@ const Form = () => {
     e.preventDefault();
 
     emailjs.sendForm(
-      process.env.SERVICE_ID,
-      process.env.TEMPLATE_ID,
+      process.env.REACT_APP_SERVICE_ID,
+      process.env.REACT_APP_TEMPLATE_ID,
       form.current,
-      process.env.PUBLIC_KEY,
+      process.env.REACT_APP_PUBLIC_KEY,
       )
       .then((result) => {
           console.log(result.text);
